fb1923467a50d9b0b4f9eeaa534954f2fa7ded45
edenyav <edenyav@post.bgu.ac.il>
2020-10-31 22:26:51 +0200

    Supplier and Product classes were added

---
+++ src/Product.java
@@ -0,0 +1,74 @@
+import com.sun.javaws.exceptions.InvalidArgumentException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Product {
+    private String id;
+    private String name;
+    private Supplier sup;
+    private List<LineItem> items;
+    private PremiumAccount premiumAccount;
+
+    /**
+     * Product constructor -> includes id and name and must have a supplier
+     * @param id - string
+     * @param name - string
+     * @param sup - supplier
+     * @throws InvalidArgumentException
+     */
+    public Product(String id, String name, Supplier sup) throws InvalidArgumentException{
+        this.id = id;
+        this.name = name;
+        this.sup = sup;
+        setSupplier(sup);
+        this.items = new ArrayList<>();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Supplier getSup() {
+        return sup;
+    }
+
+    public List<LineItem> getItems() {
+        return items;
+    }
+
+    public PremiumAccount getPremiumAccount() {
+        return premiumAccount;
+    }
+
+    /**
+     * supplier set -> product has to have exactly one supplier.
+     * @param sup-Supplier
+     * @throws InvalidArgumentException
+     */
+    public void setSupplier(Supplier sup) throws InvalidArgumentException {
+        if(sup == null)
+            throw new InvalidArgumentException(new String[]{"product must be related to one supplier"});
+        this.sup = sup;
+    }
+
+    /**
+     * product can have one premium account or none (no input validation is required)
+     * @param premiumAccount -PremiumAccount
+     */
+    public void setPremiumAccount(PremiumAccount premiumAccount) {
+        this.premiumAccount = premiumAccount;
+    }
+
+    /**
+     * add items to the LineItem list (initiated int the constructor)
+     * @param item - LineItem
+     */
+    public void addLineItem(LineItem item){
+        this.items.add(item);
+    }
+}
---
+++ src/Supplier.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Supplier {
+
+    private String id;
+    private String name;
+    private List<Product> products;
+
+    /**
+     * supplier constructor -> includes id and name
+     * @param id - string
+     * @param name - string
+     */
+    public Supplier(String id, String name) {
+        this.id = id;
+        this.name = name;
+        this.products = new ArrayList<>();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    /**
+     * add a product to the Products list (optional *)
+     * @param product - Product
+     */
+    public void addProduct(Product product){
+        this.products.add(product);
+    }
+}
