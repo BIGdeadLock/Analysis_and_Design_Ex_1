40459d0214705365a3e962aaec8574788a1c5538
edenyav <edenyav@post.bgu.ac.il>
2020-12-27 22:59:54 +0200

    CreditCardCompany was added

---
+++ Ex4/src/CreditCardCompany.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class CreditCardCompany {
+
+    HashMap<String, Integer> creditCards;
+
+    public CreditCardCompany() {
+        this.creditCards = new HashMap<String, Integer>();
+    }
+
+    public void ChargeCard(double amount){
+
+    }
+}
