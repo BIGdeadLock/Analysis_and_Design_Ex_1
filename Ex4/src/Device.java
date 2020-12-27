d9a7ed39d0af440f475e1aa808bb56004344c990
RozeKop <rozelin101@gmail.com>
2020-12-27 17:00:25 +0200

    device + e-ticket

--- Ex4/src/Device.java
+++ Ex4/src/Device.java
@@ -1,7 +1,38 @@
 public class Device {
+    String name;
     Double minAge;
     Double minHeight;
     Double minWeight;
     boolean open;
 
+    public Device(String name,Double minAge,Double minHeight,Double minWight, boolean open){
+        this.name = name;
+        this.minAge = minAge;
+        this.minHeight = minHeight;
+        this.minWeight = minWight;
+        this.open = open;
+    }
+    public Device(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getMinAge() {
+        return minAge;
+    }
+
+    public Double getMinHeight() {
+        return minHeight;
+    }
+
+    public Double getMinWeight() {
+        return minWeight;
+    }
+
+    public boolean isOpen() {
+        return open;
+    }
 }
--- Ex4/src/ETicket.java
+++ Ex4/src/ETicket.java
@@ -10,4 +10,49 @@ public class ETicket {
     ArrayList<Device> extremeDevicesPermitted = new ArrayList<Device>();
     Double childWeight;
 
+
+    public Date getEntrace() {
+        return Entrace;
+    }
+
+    public Double getTimeLeft() {
+        return timeLeft;
+    }
+
+    public ArrayList<Device> getDevicesAllowed() {
+        return devicesAllowed;
+    }
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public Double getChildHeight() {
+        return childHeight;
+    }
+
+    public ArrayList<Device> getExtremeDevicesPermitted() {
+        return extremeDevicesPermitted;
+    }
+
+    public Double getChildWeight() {
+        return childWeight;
+    }
+
+    public void addRide(Device device){
+        devicesAllowed.add(device);
+    }
+
+    public void removeRide(String name){
+        int flag = 0;
+        for (Device dev : devicesAllowed) {
+            if (dev.getName().equals(name.toLowerCase())){
+                devicesAllowed.remove(dev);
+                flag = 1;
+                System.out.println("the " + name.toLowerCase() + " device was removed successfully");
+
+            }
+        }
+
+    }
 }
