8bea911f9a69035b3e8301300b049138db1fd7fe
edenyav <edenyav@post.bgu.ac.il>
2020-12-27 21:51:48 +0200

    US4 - started

--- Ex4/src/System.java
+++ Ex4/src/System.java
@@ -1,8 +1,26 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class System {
     HashMap<String, Child> childUsers = new HashMap<String, Child>();
     HashMap<String, String> ID_Password = new HashMap<String, String>();
 
+    ArrayList<ETicket> tickets = new ArrayList<ETicket>();
+    ArrayList<EBracelet> bracelets = new ArrayList<EBracelet>();
+    ArrayList<Guardian> users = new ArrayList<Guardian>();
 
+    public void addToTicket(String rideName){
+        Double ridePrice = 0.0;
+        for(ETicket eticket: tickets){
+            for(Device device: eticket.getDevicesAllowed())
+        }
+    }
+
+    public void removeToTicket(String rideName){
+
+    }
+
+    private boolean checkUserAccountLimit(Double priceToAdd){
+
+    }
 }
